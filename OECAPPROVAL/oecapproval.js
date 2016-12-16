f0c2963183a93b98fd79b413233dc4256facd38b
shurley1972 <shawn.hurley@hurleytech.onmicrosoft.com>
2016-12-15 18:50:09 -0600

    Added on workflow remove rejection validation on approve

--- OECAPPROVAL/oecapproval.js
+++ OECAPPROVAL/oecapproval.js
@@ -87,6 +87,7 @@ define(['text!./oecapproval.html', 'text!./workflow.xaml'], function( htmlString
 			$(".custom-error-text").hide();
 			this.runtime()._closeOnButtonClick = this.closeFormOnButtonClick;
 			//this.approvalWorkflowJSON()[this.ID] = this.$jsonNewEmptyWorkflowStep();
+			$(".custom-error-text").hide();	
 			this.approvalWorkflowJSON()[this.ID].ReviewerOutcome = "Approved";
 			this.approvalWorkflowJSON()[this.ID].ReviewerComments = this.commentsValue();
 			this.approvalWorkflowJSON.valueHasMutated();
